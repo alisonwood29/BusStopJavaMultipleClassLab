68d2d28fafe6c8c16f9dba9e6e81fab7c8b427ef
Alison Wood <alisonwood29@gmail.com>
2018-01-09 13:59:40 +0000

    added test for bus starts empty

--- src/test/java/BusTest.java
+++ src/test/java/BusTest.java
@@ -1,4 +1,21 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class BusTest {
 
-    p
+    Bus bus;
+    Person person;
+
+    @Before
+    public void before(){
+        bus = new Bus("Granton", 60);
+        person = new Person();
+    }
+
+    @Test
+    public void busStartsEmpty(){
+        assertEquals(0, bus.passengerCount());
+    }
 }
