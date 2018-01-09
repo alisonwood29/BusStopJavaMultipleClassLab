f563f8b3790d4eb8f8ec318d432f1ecb9b6a5a17
Alison Wood <alisonwood29@gmail.com>
2018-01-09 14:23:33 +0000

    created tests for adding a passenger if bus is under capacity and tests for if bus is full

--- src/test/java/BusTest.java
+++ src/test/java/BusTest.java
@@ -18,4 +18,20 @@ public class BusTest {
     public void busStartsEmpty(){
         assertEquals(0, bus.passengerCount());
     }
+
+    @Test
+    public void busCanTakePassenger(){
+        bus.addPassenger(person);
+        assertEquals(1, bus.passengerCount());
+    }
+
+    @Test
+    public void busIsFull(){
+       Bus bus1 = new Bus("Bruntsfield", 1);
+        Person person1 = new Person();
+        Person person2 = new Person();
+        bus1.addPassenger(person1);
+        bus1.addPassenger(person2);
+        assertEquals(1, bus1.passengerCount());
+    }
 }
